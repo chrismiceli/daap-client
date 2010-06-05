6cbaf1d003fa1b3c697b54c948398c75a9b230d1
michael.miceli88 <michael.miceli88@24a842de-7c49-55e2-679a-eefb1045810d>
2010-06-05 14:17:21 +0000

    make directory

--- branches/src/org/mult/daap/client/SongTrackComparator.java
+++
@@ -1,9 +0,0 @@
-package org.mult.daap.client;
-
-import java.util.Comparator;
-
-public class SongTrackComparator implements Comparator<Song> {
-   public int compare(Song s1, Song s2) {
-      return (s1.track - s2.track);
-   }
-}
