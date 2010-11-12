04ad0f4e3e8297ccabb8a82726e29061ffbb3d8e
chrismiceli <chrismiceli@24a842de-7c49-55e2-679a-eefb1045810d>
2010-11-12 17:49:07 +0000

    Replaced hand rolled buffering with Android's native buffering. This should fix some response issues and shuttering of songs.

---
+++ src/org/mult/daap/FileUtils.java
@@ -0,0 +1,28 @@
+package org.mult.daap;
+
+import java.io.File;
+
+public class FileUtils {
+    static public boolean deleteDirectory(File path) {
+        if (path.exists()) {
+            File[] files = path.listFiles();
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].isDirectory()) {
+                    deleteDirectory(files[i]);
+                }
+                else {
+                    files[i].delete();
+                }
+            }
+        }
+        return (path.delete());
+    }
+
+    static void deleteIfExists(File file) {
+        if (file != null) {
+            if (file.exists() == true) {
+                file.delete();
+            }
+        }
+    }
+}
--- src/org/mult/daap/PlaylistBrowser.java
+++ src/org/mult/daap/PlaylistBrowser.java
@@ -26,166 +26,171 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class PlaylistBrowser extends Activity implements Observer {
-	private ListView playlistList;
-	private int count;
-	public final static int START = 0;
-	public final static int FINISHED = 1;
-	public final static int EMPTY = 2;
-	public final static int INITIALIZED = 3;
-	public final static int MENU_STOP = 4;
-	private ArrayList<DaapPlaylist> l;
-	private ProgressDialog pd = null;
+    private ListView playlistList;
+    private int count;
+    public final static int START = 0;
+    public final static int FINISHED = 1;
+    public final static int EMPTY = 2;
+    public final static int INITIALIZED = 3;
+    public final static int MENU_STOP = 4;
+    private ArrayList<DaapPlaylist> l;
+    private ProgressDialog pd = null;
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setResult(Activity.RESULT_OK);
-		if (Contents.address == null) {
-			// We got kicked out of memory probably
-			Contents.clearState();
-			Contents.clearLists();
-			NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-			notificationManager.cancelAll();
-			setResult(Activity.RESULT_CANCELED);
-			finish();
-			return;
-		}
-		this.setContentView(R.xml.playlist_browser);
-		l = new ArrayList<DaapPlaylist>(Contents.daapHost.getPlaylists());
-		l.add(0, new DaapPlaylist(Contents.daapHost,
-				getString(R.string.all_songs), true));
-		count = l.size();
-		playlistList = (ListView) findViewById(R.id.playlistList);
-		playlistList.setAdapter(new ProfilesAdapter(getApplicationContext()));
-		playlistList.setOnItemClickListener(playlistGridListener);
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setResult(Activity.RESULT_OK);
+        if (Contents.address == null) {
+            // We got kicked out of memory probably
+            MediaPlayback.clearState();
+            Contents.clearLists();
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.cancelAll();
+            setResult(Activity.RESULT_CANCELED);
+            finish();
+            return;
+        }
+        this.setContentView(R.xml.playlist_browser);
+        l = new ArrayList<DaapPlaylist>(Contents.daapHost.getPlaylists());
+        l.add(0, new DaapPlaylist(Contents.daapHost,
+                getString(R.string.all_songs), true));
+        count = l.size();
+        playlistList = (ListView) findViewById(R.id.playlistList);
+        playlistList.setAdapter(new ProfilesAdapter(getApplicationContext()));
+        playlistList.setOnItemClickListener(playlistGridListener);
+    }
 
-	@Override
-	public void onResume() {
-		super.onResume(); // this.position = position;
-		GetSongsForPlaylist gsfp = Contents.getSongsForPlaylist;
-		if (gsfp != null) {
-			gsfp.addObserver(this);
-			// Since lm is not null, we have to create a new pd
-			Integer lastMessage = gsfp.getLastMessage();
-			if (lastMessage == INITIALIZED) {
-				update(gsfp, START);
-			} else {
-				update(gsfp, lastMessage);
-			}
-		}
-	}
+    @Override
+    public void onResume() {
+        super.onResume(); // this.position = position;
+        GetSongsForPlaylist gsfp = Contents.getSongsForPlaylist;
+        if (gsfp != null) {
+            gsfp.addObserver(this);
+            // Since lm is not null, we have to create a new pd
+            Integer lastMessage = gsfp.getLastMessage();
+            if (lastMessage == INITIALIZED) {
+                update(gsfp, START);
+            }
+            else {
+                update(gsfp, lastMessage);
+            }
+        }
+    }
 
-	public void onDestroy() {
-		super.onDestroy();
-		if (pd != null) {
-			pd.dismiss();
-		}
-		if (Contents.getSongsForPlaylist != null) {
-			Contents.getSongsForPlaylist.deleteObserver(this);
-		}
-	}
+    public void onDestroy() {
+        super.onDestroy();
+        if (pd != null) {
+            pd.dismiss();
+        }
+        if (Contents.getSongsForPlaylist != null) {
+            Contents.getSongsForPlaylist.deleteObserver(this);
+        }
+    }
 
-	private OnItemClickListener playlistGridListener = new OnItemClickListener() {
-		public void onItemClick(AdapterView<?> parent, View v, int position,
-				long id) {
-			try {
-				if (Contents.playlist_position != position) {
-					// clicking new list
-					GetSongsForPlaylist gsfp = new GetSongsForPlaylist(
-							l.get(position));
-					grabSongs(gsfp);
-					Contents.playlist_position = (short) position;
-				} else {
-					uiHandler.sendEmptyMessage(FINISHED);
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	};
+    private OnItemClickListener playlistGridListener = new OnItemClickListener() {
+        public void onItemClick(AdapterView<?> parent, View v, int position,
+                long id) {
+            try {
+                if (Contents.playlist_position != position) {
+                    // clicking new list
+                    GetSongsForPlaylist gsfp = new GetSongsForPlaylist(
+                            l.get(position));
+                    grabSongs(gsfp);
+                    Contents.playlist_position = (short) position;
+                }
+                else {
+                    uiHandler.sendEmptyMessage(FINISHED);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    };
 
-	public void update(Observable observable, Object data) {
-		if (((Integer) data).compareTo(START) == 0) {
-			pd = ProgressDialog.show(this,
-					getString(R.string.fetching_music_title),
-					getString(R.string.fetching_music_detail), true, false);
-		} else if (((Integer) data).compareTo(FINISHED) == 0) {
-			uiHandler.sendEmptyMessage(FINISHED);
-		} else if (((Integer) data).compareTo(EMPTY) == 0) {
-			uiHandler.sendEmptyMessage(EMPTY);
-		}
-	}
+    public void update(Observable observable, Object data) {
+        if (((Integer) data).compareTo(START) == 0) {
+            pd = ProgressDialog.show(this,
+                    getString(R.string.fetching_music_title),
+                    getString(R.string.fetching_music_detail), true, false);
+        }
+        else if (((Integer) data).compareTo(FINISHED) == 0) {
+            uiHandler.sendEmptyMessage(FINISHED);
+        }
+        else if (((Integer) data).compareTo(EMPTY) == 0) {
+            uiHandler.sendEmptyMessage(EMPTY);
+        }
+    }
 
-	private Handler uiHandler = new Handler() {
-		@Override
-		public void handleMessage(Message msg) {
-			if (msg.what == FINISHED) { // Finished
-				if (pd != null) {
-					pd.dismiss();
-				}
-				Contents.getSongsForPlaylist = null;
-				Intent intent = new Intent(PlaylistBrowser.this, TabMain.class);
-				startActivityForResult(intent, 1);
-			} else if (msg.what == EMPTY) {
-				if (pd != null) {
-					pd.dismiss();
-				}
-				Contents.getSongsForPlaylist = null;
-				Toast tst = Toast.makeText(PlaylistBrowser.this,
-						getString(R.string.empty_playlist), Toast.LENGTH_LONG);
-				tst.setGravity(Gravity.CENTER, tst.getXOffset() / 2,
-						tst.getYOffset() / 2);
-				tst.show();
-			}
-		}
-	};
+    private Handler uiHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == FINISHED) { // Finished
+                if (pd != null) {
+                    pd.dismiss();
+                }
+                Contents.getSongsForPlaylist = null;
+                Intent intent = new Intent(PlaylistBrowser.this, TabMain.class);
+                startActivityForResult(intent, 1);
+            }
+            else if (msg.what == EMPTY) {
+                if (pd != null) {
+                    pd.dismiss();
+                }
+                Contents.getSongsForPlaylist = null;
+                Toast tst = Toast.makeText(PlaylistBrowser.this,
+                        getString(R.string.empty_playlist), Toast.LENGTH_LONG);
+                tst.setGravity(Gravity.CENTER, tst.getXOffset() / 2,
+                        tst.getYOffset() / 2);
+                tst.show();
+            }
+        }
+    };
 
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		if (resultCode == Activity.RESULT_CANCELED) {
-			setResult(Activity.RESULT_CANCELED);
-			finish();
-		}
-	}
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_CANCELED) {
+            setResult(Activity.RESULT_CANCELED);
+            finish();
+        }
+    }
 
-	public void grabSongs(GetSongsForPlaylist gsfp) {
-		NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-		notificationManager.cancelAll();
-		Contents.clearLists();
-		Contents.clearState();
-		Contents.getSongsForPlaylist = gsfp;
-		gsfp.addObserver(this);
-		Thread thread = new Thread(gsfp);
-		thread.start();
-		update(gsfp, START);
-	}
+    public void grabSongs(GetSongsForPlaylist gsfp) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.cancelAll();
+        Contents.clearLists();
+        MediaPlayback.clearState();
+        Contents.getSongsForPlaylist = gsfp;
+        gsfp.addObserver(this);
+        Thread thread = new Thread(gsfp);
+        thread.start();
+        update(gsfp, START);
+    }
 
-	public class ProfilesAdapter extends BaseAdapter {
-		private Context vContext;
+    public class ProfilesAdapter extends BaseAdapter {
+        private Context vContext;
 
-		public ProfilesAdapter(Context c) {
-			vContext = c;
-		}
+        public ProfilesAdapter(Context c) {
+            vContext = c;
+        }
 
-		public int getCount() {
-			return count;
-		}
+        public int getCount() {
+            return count;
+        }
 
-		public Object getItem(int position) {
-			return position;
-		}
+        public Object getItem(int position) {
+            return position;
+        }
 
-		public long getItemId(int position) {
-			return position;
-		}
+        public long getItemId(int position) {
+            return position;
+        }
 
-		public View getView(int position, View convertView, ViewGroup parent) {
-			TextView tv = new TextView(vContext.getApplicationContext());
-			tv.setTextSize(24);
-			// tv.setTextColor(Color.rgb(0x00, 0x00, 0x00));
-			tv.setText(l.get(position).getName());
-			return tv;
-		}
-	}
+        public View getView(int position, View convertView, ViewGroup parent) {
+            TextView tv = new TextView(vContext.getApplicationContext());
+            tv.setTextSize(24);
+            // tv.setTextColor(Color.rgb(0x00, 0x00, 0x00));
+            tv.setText(l.get(position).getName());
+            return tv;
+        }
+    }
 }
