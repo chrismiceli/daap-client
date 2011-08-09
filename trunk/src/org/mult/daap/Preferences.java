21fd226618414c38949fdaae3c341323ee659c58
michael.miceli88 <michael.miceli88@24a842de-7c49-55e2-679a-eefb1045810d>
2011-08-09 22:31:49 +0000

    scrobbler support

--- trunk/src/org/mult/daap/Preferences.java
+++ trunk/src/org/mult/daap/Preferences.java
@@ -2,6 +2,7 @@ package org.mult.daap;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.Preference.OnPreferenceChangeListener;
@@ -9,28 +10,44 @@ import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
 
 public class Preferences extends PreferenceActivity {
-    private ListPreference fontSizePref;
-    private SharedPreferences mPrefs;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
-        addPreferencesFromResource(R.layout.preferences_screen);
-        fontSizePref = (ListPreference) getPreferenceScreen().findPreference(
-                "font_pref");
-        fontSizePref
-                .setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
-                    @Override
-                    public boolean onPreferenceChange(Preference preference,
-                            Object newValue) {
-                        fontSizePref.setValue((String) newValue);
-                        SharedPreferences.Editor ed = mPrefs.edit();
-                        ed.putInt("font_size",
-                                Integer.valueOf((String) newValue));
-                        ed.commit();
-                        return false;
-                    }
-                });
-    }
+	private ListPreference fontSizePref;
+	private SharedPreferences mPrefs;
+	private CheckBoxPreference scrobblerPref;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+		addPreferencesFromResource(R.layout.preferences_screen);
+		fontSizePref = (ListPreference) getPreferenceScreen().findPreference(
+				"font_pref");
+		scrobblerPref = (CheckBoxPreference) getPreferenceScreen().findPreference(
+		"scrobbler_pref");
+		fontSizePref
+				.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						fontSizePref.setValue((String) newValue);
+						SharedPreferences.Editor ed = mPrefs.edit();
+						ed.putInt("font_size",
+								Integer.valueOf((String) newValue));
+						ed.commit();
+						return false;
+					}
+				});
+		scrobblerPref
+				.setOnPreferenceChangeListener(new OnPreferenceChangeListener() {
+					public boolean onPreferenceChange(Preference preference,
+							Object newValue) {
+						scrobblerPref.setChecked((Boolean) newValue);
+						SharedPreferences.Editor ed = mPrefs.edit();
+						ed.putBoolean("scrobbler_pref",
+								Boolean.valueOf((Boolean) newValue));
+						ed.commit();
+						return false;
+					}
+				});
+
+	}
 }
