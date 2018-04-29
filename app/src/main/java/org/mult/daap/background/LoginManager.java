dc4925acfeb07c4cef20d499c11faaad69b73a15
Chris Miceli <chrismiceli@users.noreply.github.com>
2018-04-28 21:30:58 -0500

    Check for a server in the db before connecting (#101)

---
+++ app/src/main/java/org/mult/daap/background/LoginManager.java
@@ -0,0 +1,91 @@
+package org.mult.daap.background;
+
+import android.os.AsyncTask;
+
+import org.mult.daap.AddServerMenu;
+import org.mult.daap.Contents;
+import org.mult.daap.client.Host;
+import org.mult.daap.client.daap.exception.PasswordFailedException;
+import org.mult.daap.db.entity.ServerEntity;
+
+import java.lang.ref.WeakReference;
+import java.net.InetAddress;
+
+public class LoginManager extends AsyncTask<Void, Integer, Integer> {
+    final public static int INITIATED = 1;
+    final public static int CONNECTION_FINISHED = 2;
+    final private static int ERROR = 3;
+    final public static int PASSWORD_FAILED = 4;
+    final public ServerEntity server;
+    final private WeakReference<AddServerMenu> addServerMenu;
+
+    public LoginManager(AddServerMenu addServerMenu, ServerEntity server) {
+        this.addServerMenu = new WeakReference<>(addServerMenu);
+        this.server = server;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        AddServerMenu addServerMenu = this.addServerMenu.get();
+        if (addServerMenu != null && !addServerMenu.isFinishing()) {
+            addServerMenu.onBeforeLogin();
+        }
+    }
+
+    @Override
+    protected Integer doInBackground(Void... params) {
+        // login to the DAAP server, if successful server is stored in database
+        try {
+            String[] urlAddress = this.server.getAddress().split(":");
+            String hostname = urlAddress[0];
+
+            Contents.address = InetAddress.getByName(hostname);
+
+            if (Contents.daapHost != null) {
+                try {
+                    Contents.daapHost.logout();
+                    Contents.playlist_position = -1;
+                } catch (Exception e) {
+                    // do nothing
+                }
+
+                Contents.clearLists();
+            }
+
+            Contents.daapHost = new Host(this.server.getPassword(), Contents.address, this.server.getPort());
+
+            try {
+                Contents.daapHost.connect();
+            } catch (PasswordFailedException e) {
+                publishProgress(LoginManager.PASSWORD_FAILED);
+                return LoginManager.PASSWORD_FAILED;
+            }
+        } catch (Exception e) {
+            publishProgress(LoginManager.ERROR);
+            return LoginManager.ERROR;
+        }
+
+        publishProgress(LoginManager.CONNECTION_FINISHED);
+        return LoginManager.CONNECTION_FINISHED;
+    }
+
+    @Override
+    protected void onProgressUpdate(Integer... values) {
+        super.onProgressUpdate(values);
+        int state = values[0];
+        AddServerMenu addServerMenu = this.addServerMenu.get();
+        if (addServerMenu != null && !addServerMenu.isFinishing()) {
+            addServerMenu.onLoginUpdate(state, this.server);
+        }
+    }
+
+    @Override
+    protected void onPostExecute(Integer result) {
+        super.onPostExecute(result);
+        AddServerMenu addServerMenu = this.addServerMenu.get();
+        if (addServerMenu != null && !addServerMenu.isFinishing()) {
+                addServerMenu.onAfterLogin(result);
+            }
+    }
+}
--- app/src/main/java/org/mult/daap/db/dao/ServerDao.java
+++ app/src/main/java/org/mult/daap/db/dao/ServerDao.java
@@ -2,6 +2,7 @@ package org.mult.daap.db.dao;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
@@ -11,7 +12,7 @@ import org.mult.daap.db.entity.ServerEntity;
 @Dao
 public interface ServerDao {
     @Query("SELECT * FROM servers")
-    LiveData<ServerEntity[]> loadAllServers();
+    ServerEntity[] loadAllServers();
 
     /**
      * Sets the single DAAP server to use for the app.
@@ -21,4 +22,7 @@ public interface ServerDao {
      */
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void setDaapServer(ServerEntity server);
+
+    @Delete
+    public void deleteServers(ServerEntity... users);
 }
